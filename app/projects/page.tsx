9a74ebe20f185cff9fff48272059975abb9253ea
Mohamed Elshazly <22671838+MohamedElshazly@users.noreply.github.com>
2023-07-02 06:20:37 +0300

    responsive view for projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card"
 import { Button, buttonVariants } from '@/components/ui/button';
 import Image from 'next/image'
@@ -15,19 +15,19 @@ const Projects = () => {
 	return (
 		<div className="z-20 flex flex-col items-center justify-center gap-10 font-mono">
 			<h1 className='mt-16 text-3xl'>My Projects</h1>
-			<div className="mb-5 grid grid-cols-3 gap-5">
+			<div className="mb-5 grid gap-5 md:grid-cols-2 xl:grid-cols-3">
 
-				<Card className="flex w-96 flex-col">
+				<Card className="relative flex w-96 flex-col">
 					<CardHeader>
 						<CardTitle className="flex justify-center text-primary">Post Rock</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<CardDescription className="flex flex-col gap-5"> 
+						<CardDescription className="mb-20 flex flex-col gap-5">
 							<Image alt="p" src={"/p1.png"} width={320} height={120} />
 							Post Rock is a website utilizing Spotify web API to get every song in the Post-Rock genre, it allows users to search for any post-rock band they want, and view all of their albums and tracks. Users can play the preview of the songs, then they'd be redirected to Spotify to listen to the full song with a click of a button.
 						</CardDescription>
 					</CardContent>
-					<CardFooter className="flex justify-between justify-self-end">
+					<CardFooter className="absolute bottom-0 flex w-full justify-between">
 						<Link href="https://post-rock-10e9e.web.app/" target="_blank" className={buttonVariants({ variant: "outline", size: "sm", className: "bg-primary dark:bg-secondary dark:hover:bg-secondary-foreground dark:hover:text-primary" })}>View Project</Link>
 						<Link href="https://github.com/MohamedElshazly/Post-Rock" target="_blank" className={buttonVariants({ variant: "outline", size: "sm", className: "bg-primary dark:bg-secondary dark:hover:bg-secondary-foreground dark:hover:text-primary" })}>View Code</Link>
 					</CardFooter>
@@ -38,8 +38,8 @@ const Projects = () => {
 						<CardTitle className="flex justify-center text-primary">Kanban board</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<CardDescription className="flex flex-col gap-5">
-							<Image alt="p" src={"/kanban.png"} width={320} height={120}/>
+						<CardDescription className="mb-20 flex flex-col gap-5">
+							<Image alt="p" src={"/kanban.png"} width={320} height={120} />
 							Kanban board is a website that allows users to add tasks to a kanban board, and move them between columns, they can also export the tasks into JSON, and import an already existing JSON into tasks. The website is built using React.js, and React dnd.
 						</CardDescription>
 					</CardContent>
@@ -54,8 +54,8 @@ const Projects = () => {
 						<CardTitle className="flex justify-center text-primary">Javascript Calculator</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<CardDescription className="flex flex-col gap-5">
-							<Image alt="p" src={"/calculator.png"} width={320} height={120}/>
+						<CardDescription className="mb-20 flex flex-col gap-5">
+							<Image alt="p" src={"/calculator.png"} width={320} height={120} />
 							A simple calculator built using Javascript. It allows users to do simple calculations.
 						</CardDescription>
 					</CardContent>
@@ -65,18 +65,18 @@ const Projects = () => {
 					</CardFooter>
 				</Card>
 
-				<Card className="flex w-96 flex-col">
+				<Card className="relative flex w-96 flex-col">
 					<CardHeader>
 						<CardTitle className="flex justify-center text-primary">Etch a Sketch</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<CardDescription className="flex flex-col gap-5">
-							<Image alt="p" src={"/etch.png"} width={320} height={120}/>
-							A recreation of the game 'Etch A Sketch' on the web, where you have two modes, drawing and deleting. You can also resize and reset the canvas however you want.  
-							In this project, I aimed to demonstrate my DOM manipulation skills, and design.						
+						<CardDescription className="mb-20 flex flex-col gap-5">
+							<Image alt="p" src={"/etch.png"} width={320} height={120} />
+							A recreation of the game 'Etch A Sketch' on the web, where you have two modes, drawing and deleting. You can also resize and reset the canvas however you want.
+							In this project, I aimed to demonstrate my DOM manipulation skills, and design.
 						</CardDescription>
 					</CardContent>
-					<CardFooter className=" flex w-full justify-between">
+					<CardFooter className="absolute bottom-0 flex w-full justify-between">
 						<Link href="https://mohamedelshazly.github.io/Etch-A-Sketch/" target="_blank" className={buttonVariants({ variant: "outline", size: "sm", className: "bg-primary dark:bg-secondary dark:hover:bg-secondary-foreground dark:hover:text-primary" })}>View Project</Link>
 						<Link href="https://github.com/MohamedElshazly/Etch-A-Sketch" target="_blank" className={buttonVariants({ variant: "outline", size: "sm", className: "bg-primary dark:bg-secondary dark:hover:bg-secondary-foreground dark:hover:text-primary" })}>View Code</Link>
 					</CardFooter>
@@ -87,9 +87,9 @@ const Projects = () => {
 						<CardTitle className="flex justify-center text-primary">Library</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<CardDescription className="flex flex-col gap-5">
-							<Image alt="p" src={"/library.png"} width={320} height={120}/>
-							Library is a website that allows users to add books to a library. Users can also delete books from the library, mark them as read or not, etc.						
+						<CardDescription className="mb-20 flex flex-col gap-5">
+							<Image alt="p" src={"/library.png"} width={320} height={120} />
+							Library is a website that allows users to add books to a library. Users can also delete books from the library, mark them as read or not, etc.
 						</CardDescription>
 					</CardContent>
 					<CardFooter className="absolute bottom-0 flex w-full justify-between">
@@ -103,8 +103,8 @@ const Projects = () => {
 						<CardTitle className="flex justify-center text-primary">Tic Tac Toe</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<CardDescription className="flex flex-col gap-5">
-							<Image alt="p" src={"/tic.png"} width={320} height={120}/>
+						<CardDescription className="mb-20 flex flex-col gap-5">
+							<Image alt="p" src={"/tic.png"} width={320} height={120} />
 							Simple Tic Tac Toe game built using Javascript.
 						</CardDescription>
 					</CardContent>
